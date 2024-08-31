ab4749330bb9fb54f903d67527569d5c43410fa8
Mehul Gupta <mehulgupta2409@Mehuls-MacBook-Air.local>
2024-08-31 17:06:25 +0530

    Initial commit

---
+++ Python_Basics/first.py
@@ -0,0 +1,95 @@
+# print(4+3)
+# print("Mehul is my name","yes")
+# print('32')
+# name="Mehul"
+# print("My name is :",name)
+# string="string hu me - "
+# print(5*string)
+# A,B="2",3
+# txt="@"
+# print((A+txt)*B)
+# concatenation
+
+# a=2
+# b=3
+# # print(a/b)
+# # here it will print 0.5 instead of 0
+# c=a//b
+# # this is an integer division,means gif funtion jesa
+# print(c,a/b)
+
+# print("")
+# b="hell"
+# print("Hello everyone my name is Mehul")
+# print("My age is 20")
+# print("I think I am master of Physics" ,b,1+2,  "as well as master of Mathematics")
+
+# print(type(b))
+# name=""
+# name1="aa"
+# realname=input()
+# print(realname)
+# print("")
+# name=int(input("what is your number : "));
+# print(53/name)
+
+
+# a=1;
+# name=bool(a);
+
+# print(name)
+
+# import keyword
+# print(keyword.kwlist)
+
+# color=input('color : ')
+# if(color == "red"):
+#    print("Stop")
+# elif(color == "green"):
+#     print("Go")
+# else:
+#     print("Look around")
+
+    
+# first=float(input("1st number is : "))
+# area=first*first
+# print(area)
+
+# str="i am ver big string. \ni play with characters"
+# print(str)
+
+# me="jeijf"
+# len1=len(me)
+# print(len1)
+# n="djen"
+# len1=len(n)
+# print(len1)
+
+# strr="I am Mehul Gupta"
+# print(strr[4:len(strr)])
+
+# str="hellao i am good"
+# print(str.count("a"))
+list_1=[1 ,2,3,4,5]
+# list_2=list_1  #coppied by references ,exact addredd is given to list_2
+# # list_1.append(3)/
+# print(list_1)
+# print(list_2)
+# list_1.pop(4)
+# print(list_1)
+# print(list_2)
+
+# print(list_1[1])
+# tuple_1=(1,2,3)
+# # print(type(tuple_1))
+# # print(tuple_1)
+# tuple1=([12 ,3],2)
+# print(tuple1)
+# dict={}
+# print(type(dict))
+
+# for element in range()/
+
+def myfun(num):
+    print(10*num)
+myfun(10)
